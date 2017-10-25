263277c76a779c918cd28e3af42df9171a57d637
Liu Hao <lh_mouse@126.com>
2017-10-25 17:31:29 +0800

    .

--- medusa2-primary/src/proxy_session.cpp
+++ medusa2-primary/src/proxy_session.cpp
@@ -158,23 +158,14 @@ protected:
 		if(m_tunnel){
 			const AUTO(tunnel_session, boost::dynamic_pointer_cast<TunnelSession>(session->get_upgraded_session()));
 			DEBUG_THROW_ASSERT(tunnel_session);
-			if(err_code == 0){
-				tunnel_session->shutdown_read();
-				tunnel_session->shutdown_write();
-			} else {
-				unlink_and_shutdown(Poseidon::Http::ST_BAD_GATEWAY, err_code, err_msg.c_str());
-			}
+			unlink_and_shutdown(Poseidon::Http::ST_BAD_GATEWAY, err_code, err_msg.c_str());
 		} else {
 			const AUTO(deaf_session, boost::dynamic_pointer_cast<DeafSession>(session->get_upgraded_session()));
 			DEBUG_THROW_ASSERT(deaf_session);
 			if(is_content_till_eof()){
 				terminate_content();
 			}
-			if(err_code == 0){
-				unlink_and_shutdown(Poseidon::Http::ST_BAD_GATEWAY, Protocol::ERR_ORIGIN_EMPTY_RESPONSE, "The origin server sent no data");
-			} else {
-				unlink_and_shutdown(Poseidon::Http::ST_BAD_GATEWAY, err_code, err_msg.c_str());
-			}
+			unlink_and_shutdown(Poseidon::Http::ST_BAD_GATEWAY, (err_code == 0) ? Protocol::ERR_ORIGIN_EMPTY_RESPONSE : 0, (err_code == 0) ? "The origin server sent no data" : "");
 		}
 	}
 
