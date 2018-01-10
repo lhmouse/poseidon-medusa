55c4d49c11d7aa559298b6c4c091df96c71d6b5f
Liu Hao <lh_mouse@126.com>
2018-01-10 17:40:12 +0800

    Use `--addso=yes` when making DEB packages.

--- makedeb.sh
+++ makedeb.sh
@@ -4,4 +4,5 @@ _pkgname="medusa2"
 _maintainer=$(git config --get user.email)
 _pkgversion=$(printf "0.%u.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
 
-sudo checkinstall --backup=no --nodoc -y --strip=no --stripso=no --pkgname="$_pkgname" --maintainer="$_maintainer" --pkgversion="$_pkgversion"
+sudo checkinstall --backup=no --nodoc -y --strip=no --stripso=no --addso=yes	\
+	--pkgname="$_pkgname" --maintainer="$_maintainer" --pkgversion="$_pkgversion"
